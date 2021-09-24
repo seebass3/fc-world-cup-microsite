774464a502435abdae1270c1d2a81ec42f660bac
seebass3 <sebastien.berth94@gmail.com>
2021-09-23 17:12:23 -0700

    Fix webfont loader

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 import WebFont from 'webfontloader';
 
-WebFont.load({
-    google: {
-      families: ['AmsiProNarw-Regular', 'AmsiProNarw-Ultra', 'AmsiPro-Light', 'AmsiProNarw-LightItalic', 'AmsiPro-Bold', 'AmsiPro-Thin']
+WebFont.load = {
+    custom: {
+      families: ['AmsiProNarw-Regular', 'AmsiProNarw-Ultra', 'AmsiPro-Light', 'AmsiProNarw-LightItalic', 'AmsiPro-Bold', 'AmsiPro-Thin'],
+      urls: ['src/components/typography.scss']
     }
-  });
+  };

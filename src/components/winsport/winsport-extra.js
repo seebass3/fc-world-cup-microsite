14e5d22d4762145c702faaf2c842c71fd8fa82aa
seebass3 <sebastien.berth94@gmail.com>
2021-12-22 18:03:13 -0800

    Covid-19 link on Winsport page

--- src/components/winsport/winsport-extra.js
+++ src/components/winsport/winsport-extra.js
@@ -28,21 +28,26 @@ const WinsportExtra = () => {
         setShowModal(prev => !prev)
     };
 
-    const contentCovid = <div className="bg-gray-100 p-10 h-half overflow-y-scroll">
-        <div className="flex flex-col items-center">
-            <h1 className="mb-12">COVID-19</h1>
-            <p>Canada's Covid-19 Management Plan for the Snow Rodeo Freeski Halfpipe FIS
-                World Cups with specific information to applying to the host city of Calgary
-                Alberta will be released closer to the event date.
-            </p>
-            <p>Please check back here later.</p>
-        </div>
-    </div>;
+    // const contentCovid = <div className="bg-gray-100 p-10 h-half overflow-y-scroll">
+    //     <div className="flex flex-col items-center">
+    //         <h1 className="mb-12">COVID-19</h1>
+    //         <p>Canada's Covid-19 Management Plan for the Snow Rodeo Freeski Halfpipe FIS
+    //             World Cups with specific information to applying to the host city of Calgary
+    //             Alberta will be released closer to the event date.
+    //         </p>
+    //         <p>Please check back here later.</p>
+    //     </div>
+    // </div>;
 
     return (
         <section id="page-footer" className="bg-black">
             <div className="grid grid-cols-1 lg:grid-cols-2">
-                <div className="relative extra-item__link" onClick={openModal}>
+                <a
+                    href="https://www.shredthenorth.ca/en/events/snow-rodeo/sbd/info/covid/"
+                    rel="noopener noreferrer"
+                    to
+                    target="_blank"
+                    className="relative extra-item__link">
                     <GatsbyImage
                         objectFit="cover"
                         className="h-96 extra-item__img"
@@ -52,7 +57,7 @@ const WinsportExtra = () => {
                         className="flex h-full items-center justify-center absolute top-0 left-0 right-0 bottom-0 p-10">
                         <h2 className="text-white text-center uppercase">COVID-19</h2>
                     </div>
-                </div>
+                </a>
 
                 <a
                     href="https://www.fis-ski.com/DB/freestyle-freeski/freeski/calhttps://www.fis-ski.com/DB/freestyle-freeski/moguls-aerials/calendar-results.html?eventselection=&place=&sectorcode=FS&seasoncode=2022&categorycode=WC&disciplinecode=HP&gendercode=&racedate=&racecodex=&nationcode=&seasonmonth=X-2022&saveselection=-1&seasonselection="
@@ -72,10 +77,10 @@ const WinsportExtra = () => {
                     </div>
                 </a>
             </div>
-            <Modal
+            {/* <Modal
                 showModal={showModal}
                 setShowModal={setShowModal}
-                modalContent={contentCovid}/>
+                modalContent={contentCovid}/> */}
         </section>
     )
 };
